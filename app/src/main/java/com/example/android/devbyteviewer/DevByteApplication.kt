24efeb9ccb154f4286a7b8bc4ca2a982d2c0e060
NAK VANNA <nakvannamcu@gmail.com>
2022-01-20 21:55:54 +0700

    Updated

--- app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -18,7 +18,15 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
+import android.os.Build
+import android.util.Log
+import androidx.work.*
+import com.example.android.devbyteviewer.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
@@ -26,20 +34,45 @@ import timber.log.Timber
 class DevByteApplication : Application() {
 
     // TODO (01) Create CoroutineScope variable applicationScope, using Dispatchers.Default.
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
 
     // TODO (02) Create a delayedInit() function that calls setupRecurringWork() in
     // the coroutine you defined above.
+    private fun delayedInit() {
+        applicationScope.launch {
+            Thread.sleep(4000)
+            Timber.i("After 4000 millis")
+        }
+    }
 
     // TODO (04) Create a setupRecurringWork() function and use a Builder to define a
     // repeatingRequest variable to handle scheduling work.
-
     // TODO (05) In setupRecurringWork(), get an instance of WorkManager and
     // launch call enqueuPeriodicWork() to schedule the work.
 
     // TODO (07) In setupRecurringWork(), define constraints to prevent work from occurring when
     // there is no network access or the device is low on battery.
+    private fun setupRecurringWork() {
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(
+            1,
+            TimeUnit.DAYS
+        ).setConstraints(constraints).build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME, ExistingPeriodicWorkPolicy.KEEP, repeatingRequest
+        )
+    }
 
     // TODO (08) Add the constraints to the repeatingRequest definition.
+    val constraints = Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.UNMETERED)
+        .setRequiresBatteryNotLow(true)
+        .setRequiresCharging(true)
+        .apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                setRequiresDeviceIdle(true)
+            }
+        }.build()
 
     /**
      * onCreate is called before the first screen is shown to the user.
@@ -47,9 +80,12 @@ class DevByteApplication : Application() {
      * Use it to setup any background tasks, running expensive setup operations in a background
      * thread to avoid delaying app start.
      */
-    override fun onCreate() {
+    override
+
+    fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         // TODO (03) Call delayedInit().
+        delayedInit()
     }
 }
